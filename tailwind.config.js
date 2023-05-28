1a7d799affec4978872af407ca6e929390397d8b
guillaume gernigon <guillaume.gernigon@outlook.fr>
2023-05-28 12:42:19 +0200

    config theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
+    colors: {
+      principal: "#00BBC9",
+      secondary: "#FFFFFF",
+      error: "#FF0000",
+      success: "#46D92C",
+      placeholder: "#A2A2A2",
+    },
+    fontFamily: {
+      nunito: ['Nunito', 'sans-serif']
+    },
     extend: {},
   },
   plugins: [],
-}
+};
