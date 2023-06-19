134676239977701c8fce9549f9617e3e1a118b54
Willigan49 <guillaume.gernigon@outlook.fr>
2023-06-19 16:32:30 +0200

    add datepickers

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,7 +1,11 @@
-export default function Input({ placeholder }: { placeholder: string }): JSX.Element {
+export default function Input({ placeholder, type }: { placeholder: string; type: string }): JSX.Element {
   return (
     <div className="w-full">
-      <input className="w-full rounded-xl border-2 border-primary p-2" type="text" placeholder={placeholder} />
+      <input
+        className="w-full rounded-xl border-2 border-primary p-2"
+        type={type}
+        placeholder={placeholder}
+      />
       {/* <p className="pl-3 text-xs text-error">it's incorrect</p> */}
     </div>
   );
--- src/routes/CreateEmployee.tsx
+++ src/routes/CreateEmployee.tsx
@@ -1,8 +1,14 @@
+import "react-datepicker/dist/react-datepicker.css";
+import DatePicker from "react-datepicker";
 import Stepper from "awesome-react-stepper";
-import Button from "../components/button";
+import Button from "../components/Button";
 import Input from "../components/Input";
+import { useState } from "react";
 
 export default function CreateEmployee(): JSX.Element {
+  const [birthDate, setBirthDate] = useState<Date | null>(null);
+  const [startDate, setStartDate] = useState<Date | null>(null);
+
   return (
     <div className="w-full max-w-[480px] rounded-3xl bg-secondary p-10">
       <h1 className="text-center text-xl">Create Employee</h1>
@@ -20,11 +26,23 @@ export default function CreateEmployee(): JSX.Element {
           <div>
             <p className="text-center">Informations</p>
           </div>
-          <div className="w-full flex flex-col gap-4 items-center">
-            <Input placeholder="First Name" />
-            <Input placeholder="Last Name" />
-            <Input placeholder="Date of Birth" />
-            <Input placeholder="Start Date" />
+          <div className="flex w-full flex-col items-center gap-4">
+            <Input placeholder="First Name" type="text" />
+            <Input placeholder="Last Name" type="text" />
+            <DatePicker
+              placeholderText="Date of Birth"
+              selected={birthDate}
+              onChange={(date: Date) => setBirthDate(date)}
+              className="w-full rounded-xl border-2 border-primary p-2"
+              disabledKeyboardNavigation
+            />
+            <DatePicker
+              placeholderText="Start Date"
+              selected={startDate}
+              onChange={(date: Date) => setStartDate(date)}
+              className="w-full rounded-xl border-2 border-primary p-2"
+              disabledKeyboardNavigation
+            />
           </div>
         </div>
         <div className="flex flex-col items-center gap-10 py-10">
@@ -32,11 +50,11 @@ export default function CreateEmployee(): JSX.Element {
             <h1 className="text-center text-xl">Create Employee</h1>
             <p className="text-center">Adress</p>
           </div>
-          <div className="w-full flex flex-col gap-4 items-center">
-            <Input placeholder="Street" />
-            <Input placeholder="City" />
-            <Input placeholder="State" />
-            <Input placeholder="ZIP Code" />
+          <div className="flex w-full flex-col items-center gap-4">
+            <Input placeholder="Street" type="text" />
+            <Input placeholder="City" type="text" />
+            <Input placeholder="State" type="text" />
+            <Input placeholder="ZIP Code" type="number" />
           </div>
         </div>
         <div className="flex flex-col items-center gap-10 py-10">
@@ -44,8 +62,8 @@ export default function CreateEmployee(): JSX.Element {
             <h1 className="text-center text-xl">Create Employee</h1>
             <p className="text-center">Department</p>
           </div>
-          <div className="w-full flex flex-col gap-4 items-center">
-            <Input placeholder="Department" />
+          <div className="flex w-full flex-col items-center gap-4">
+            <Input placeholder="Department" type="text" />
           </div>
         </div>
       </Stepper>
