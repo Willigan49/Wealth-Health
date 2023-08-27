9fc724125afa77a9015285e4e6a4f35f7aaf1aa1
Willigan49 <guillaume.gernigon@outlook.fr>
2023-08-27 11:39:00 +0200

    add filter entries

--- src/routes/TableEmployee.tsx
+++ src/routes/TableEmployee.tsx
@@ -1,8 +1,10 @@
-import { useMemo, useState } from "react";
+import { useCallback, useMemo, useState } from "react";
 import { AgGridReact } from "ag-grid-react";
 import { useSelector } from "react-redux";
 import { ColDef, GridOptions } from "ag-grid-community";
 import { type Employee } from "../enum/types";
+import { Select } from "select-component-oc";
+import entries from "../utils/entries.json";
 
 import "ag-grid-community/styles/ag-grid.css";
 import "ag-grid-community/styles/ag-theme-alpine.css";
@@ -17,7 +19,6 @@ export default function TableEmployee() {
       resizable: true,
       sortable: true,
       unSortIcon: true,
-
     };
   }, []);
   const [colDefs] = useState<ColDef<Employee>[]>([
@@ -42,9 +43,31 @@ export default function TableEmployee() {
     suppressMovableColumns: true,
   };
 
+  const onFilterTextBoxChanged = useCallback(() => {
+    gridOptions.api?.setQuickFilter((document.getElementById("filter-text-box") as HTMLInputElement).value);
+  }, []);
+
+  const onFilterNumberEntries = useCallback((e: any) => {
+    gridOptions.api?.paginationSetPageSize(e.target.value);
+  }, []);
+
   return (
-    <div className="ag-theme-alpine" style={{ height: 600, width: "100%" }}>
-      <AgGridReact gridOptions={gridOptions}></AgGridReact>
+    <div className="w-full">
+      <div className="mb-5 flex justify-between items-center">
+        <input
+          className="rounded-xl border-2 border-primary p-2 placeholder-primary placeholder-opacity-70"
+          type="text"
+          id="filter-text-box"
+          placeholder="Filter..."
+          onInput={onFilterTextBoxChanged}
+        />
+        <p className="text-secondary">
+          Show <Select name="pagination" options={entries} onChange={onFilterNumberEntries} style={{color: "black"}} /> entries
+        </p>
+      </div>
+      <div className="ag-theme-alpine" style={{ height: 600, width: "100%" }}>
+        <AgGridReact gridOptions={gridOptions}></AgGridReact>
+      </div>
     </div>
   );
 }
