698df836927882a048c19d4fe8dd782a02839149
Guillaume Gernigon <ggernigon@puydufou.com>
2023-08-31 10:50:49 +0200

    add deploy

--- vite.config.ts
+++ vite.config.ts
@@ -3,11 +3,19 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  test: {
-    environment: "jsdom",
-    setupFiles: ['./__tests__/setup.ts'],
-    globals: true,
-  },
+export default defineConfig(({ command }) => {
+  const config = {
+    base: "/",
+    plugins: [react()],
+    test: {
+      environment: "jsdom",
+      setupFiles: ["./__tests__/setup.ts"],
+      globals: true,
+    },
+  };
+  if (command !== "serve") {
+    config.base = "/react-vite-gh-pages/";
+  }
+
+  return config;
 });
